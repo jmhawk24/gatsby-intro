e67f2458d0a31d37c1a99f95a2545393a2f82105
Jacob Hawk <jhawk@splashfinancial.com>
2020-09-24 10:29:56 -0400

    gatsby yo

---
+++ src/pages/404.js
@@ -0,0 +1,9 @@
+import React from "react"
+import {Link} from "gatsby"
+
+export default () => (
+    <div>
+        <h1> what u looking for fool???</h1>
+        <Link to="/abou" >click me</Link>
+    </div>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,10 @@
 import React from "react"
 
-export default function Home() {
-  return <div>Hello world!</div>
-}
+export default () => (
+  <div>
+    <h1>This is index page.</h1>
+    <p>
+      Suck it.
+    </p>
+  </div>
+)
